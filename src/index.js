4d53ec6e29bd665a12f19a3ffcb3e6fa431f3beb
Rachel Lim <rachel@Rachels-MacBook-Pro.local>
2017-09-10 17:36:50 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,117 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <div>
-    </div>
+const link = {
+	width: '100px',
+	padding: '12px',
+	margin: '0 6px 6px',
+	background: 'blue',
+	textDecoration: 'none',
+	color: 'white',
+}
+
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows only to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */ 
+      style={link}
+      /* add prop for activeStyle */    
+      activeStyle={{
+      	background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+      	background:'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink 
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+      	background: 'darkblue'
+      }}
+    >Login</NavLink>          
+  </div>;
+
+const Home = () => <h1>Home</h1>
+
+const About = () => <h1>About</h1>
+
+const Login = () => 
+  <div>
+    <h1>Login</h1>
+    <form>
+      <div>
+        <input type="text" name="username" placeholder="Username" />
+        <label htmlFor="username">Username</label>
+      </div>
+      <div>
+        <input type="password" name="password" placeholder="Password" />
+        <label htmlFor="passsword">Password</label>
+      </div>
+      <input type="submit" value="Login" />
+    </form>
+  </div>
+
+  const App = () => {
+  	return (
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+  	);
+  };
+
+  ReactDOM.render (
+    <App />,
+    document.getElementById('root')
   );
-};
 
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
-);
+// const App = () => {
+//   return (
+//     <Router>
+//       <div>
+//         <Route exact path="/" render={() => <h1>Home</h1>} />
+
+//         <Route exact path="/about" render={() => <h1>About</h1>} />
+
+//         <Route exact path="/login" render={() =>
+//         <div>	
+//           <form>	
+//             <div>
+//               <input type="text" name="username" placeholder="Username" />
+//               <label htmlFor="username">Username</label>   
+//             </div>
+//             <div>
+//               <input type="password" name="password" placeholder="Password" />
+//               <label htmlFor="password">Password</label>
+//             </div>
+//             <input type="submit" value="Login" />
+//            </form>  
+//         </div>   
+//         } />
+//       </div>
+//     </Router>
+//   );
+// };
+
+// // Step 2. Changed to have router coordinate what is displayed
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
