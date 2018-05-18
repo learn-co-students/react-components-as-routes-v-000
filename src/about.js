755594d317afba3097d5ae8379cc39794879c8fa
cdhenry <cdhenry@gmail.com>
2018-05-18 15:31:22 -0400

    Done.

---
+++ src/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>This is my about component!</h2>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Login from './login'
+import About from './about'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const Home = () => {
-  return (
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Home!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
 );
