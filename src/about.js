55ed72bb6e83206bd5bf4581ccc41d7df6562dd8
Sam Ojling <s.ojling@gmail.com>
2018-05-21 11:27:06 +0200

    Done.

---
+++ src/about.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>This is my component!</h1>
+    </div>
+  );
+};
---
+++ src/login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type='text' name='username' placeholder='Username' />
+          <label htmlFor='username'>Username</label>
+        </div>
+        <div>
+          <input type='password' name='password' placeholder='Password' />
+          <label htmlFor='password'>Password</label>
+        </div>
+        <input type='submit' value='Login' />
+      </form>
+    </div>
+  );
+};
