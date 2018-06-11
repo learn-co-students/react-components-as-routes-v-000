4942ae7bdbed043bc44f64a14f7639982b0f3043
chrisjamesr <chrisjames.r@gmail.com>
2018-06-11 10:18:40 -0400

    add signup and messages components

---
+++ src/components/SignUp.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default class SignUp extends React.Component {
+  render(){
+    return(
+      <div>
+        <form>
+          <div>
+            <input type="text"
+                   name="username"
+                   placeholder="Username" />
+            <label htmlFor="username">Username</label>
+          </div>
+          <div>
+            <input type="password"
+                   placeholder="Password" />
+            <label htmlFor="password">Password</label>
+          </div>
+          <input type="submit"
+                 value="Sign Up" />
+        </form>
+      </div>  
+    )
+  }
+}
