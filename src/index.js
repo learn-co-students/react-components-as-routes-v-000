d26e45dd84af9e757e1b67dbdd880a81986ba7c1
saurookadook <maskiella@gmail.com>
2018-06-23 19:04:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import Home from './components/Home'
+import About from './About'
+import Login from './Login'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white'
+}
 
+/* add the navbar component */
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to NavLink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>
+
+// Step 2. Changed to have what router coordinate is displayed
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <div>
+      <Navbar />
+      <Route path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
