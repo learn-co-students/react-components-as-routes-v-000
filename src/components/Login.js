4a113f5b69f2c8ce13c766d4615c60f1b1ad786a
github username <yosa.yon@gmail.com>
2018-07-03 00:00:10 -0400

    Done.

---
+++ src/components/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component{
+  render(){
+    return (
+      <div>
+        <form>
+          <div>
+            <input type="text" name="username" placeholder="Username" />
+          </div>
+          <div>
+            <input type="password" name="password" placeholder="Password" />
+            <label htmlFor="password">Password</label>
+          </div>
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+      )
+    }
+  }
