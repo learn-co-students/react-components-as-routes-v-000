c17937cf582a1e155810c3cb57a0c95bf21b3996
heislercreative <brennan@heislercreative.com>
2018-07-21 13:57:45 -0600

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class Messages extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Your Messages</h1>
+        <ol>
+          <li>Welcome to React Router</li>
+          <li>Getting Started</li>
+        </ol>
+      </div>
+    )
+  }
+}
