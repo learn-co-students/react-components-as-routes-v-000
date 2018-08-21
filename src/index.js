f06dab22305b9c415c6e168922b12d421869a125
Raiet <raietsaeed@gmail.com>
2018-08-21 18:56:05 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home';
+import About from './About';
+import Login from './Login';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 ReactDOM.render(
-  <Home />,
+  // Note : Without router
+  // <Home />,
+  
+  // Note : With router
+  ( // Step 2. Changed to have router coordinate what is displayed
+  <Router>
+  
+    {/* NOTE : Need to use a fragment (<div> or <> or below) because Router requires only a *single* child/html node (as with all component returns) */}
+    <React.Fragment>
+      { /* NOTE : Using  "path" causes it to render regardless of the rest of the path (as long as THAT part exists) while using "exact path" causes those paths to render ONLY when the url exactly matches
+      <Route path="/" render={Home} />
+       */}
+       
+      <Route exact path="/" render={Home} />
+      <Route exact path="/about" render={About} />
+      <Route exact path="/login" render={Login} />
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
