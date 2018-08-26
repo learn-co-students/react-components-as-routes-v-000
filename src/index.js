95c41b9ecff4ed09b4de239f755cf6d375ee694e
GabrielValle91 <gvalle91@gmail.com>
2018-08-26 12:43:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import Home from './home'
+import About from './about'
+import Login from './login'
+import Signup from './signup'
+import Messages from './messages'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
 };
 
-const About = () => {
-  return (
+const Navbar = () => {
+  return(
     <div>
-      <h1>This is my about component!</h1>
+      <NavLink
+        to="/"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Home</NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >About</NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Login</NavLink>
+      <NavLink
+        to="/signup"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Signup</NavLink>
+      <NavLink
+        to="/messages"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Messages</NavLink>
     </div>
-  );
-};
-
-const Login = () => {
-  return (
-    <div>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
-        </div>
-        <input type="submit" value="Login" />
-      </form>
-    </div>
-  );
+  )
 };
 
 ReactDOM.render((
   <Router>
     <React.Fragment>
-      <Route path="/" render={Home} />
-      <Route exact path="/about" render={About} />
-      <Route exact path="/login" render={Login} />
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/messages" component={Messages} />
     </React.Fragment>
   </Router>),
   document.getElementById('root')
---
+++ src/messages.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Messages extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>This is my messages component!</h1>
+      </div>
+    )
+  }
+}
+
+export default Messages;
