7696f27a8f34cfe4f94cd6b0f80776c9d2c3197f
jd2rogers2 <jd2rogers2@gmail.com>
2018-08-31 23:33:51 -0400

    playing around with react router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -9,7 +10,34 @@ const Home = () => {
   );
 };
 
-ReactDOM.render(
-  <Home />,
+const Blog = () => {
+  return (
+    <div>
+      <h1>Blog!</h1>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1>About!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <ul>
+        <li><a href="http://localhost:3000/">home</a></li>
+        <li><a href="http://localhost:3000/about">about</a></li>
+        <li><a href="http://localhost:3000/blog">blog</a></li>
+      </ul>
+      <Route exact path="/" render={Home} />
+      <Route exact path="/about" render={About} />
+      <Route exact path="/blog" render={Blog} />
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
