6d00d72ff7f3758ba4ec014d8aafbf8f525a40b5
johnsontaiwo <josseytee@gmail.com>
2018-09-01 17:30:53 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import Home from './home';
+import About from './about';
+import Login from './login'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
 
-ReactDOM.render(
-  <Home />,
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      exact
+      
+      style={link}
+    
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+
+    <NavLink
+      to="/login" 
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+
+
+
+ReactDOM.render((
+  <Router>
+  <React.Fragment>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/login" component={Login} />
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
