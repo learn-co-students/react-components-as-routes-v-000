0bdc09cb9cba5fd983a69fd24a613fb3bcb9ad2e
vanetoro <vanetoro84@gmail.com>
2018-09-17 09:51:44 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const NavBar = () => 
+  <div>
+    <NavLink
+      to='/'
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+      >Home</NavLink>
+    <NavLink
+      to='/about'
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+      >About</NavLink>
+    <NavLink
+      to='/login'
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+      >Login</NavLink>
+  </div>
 
 
-const About = () => {
-  return (
-    <div>
-      <h1>This is the about compoonent!</h1>
-    </div>
-  );
-};
+const Home = () => <h1>Home!</h1>
+
+
+
+const About = () => <h1>This is the about compoonent!</h1>
+
 
 const Login = () => {
   return (
@@ -41,6 +69,7 @@ const Login = () => {
 ReactDOM.render(
   <Router>
     <React.Fragment>
+    <Route path='/' render={NavBar}/>
     <Route exact path='/' render={Home}/>
     <Route exact path='/about' render={About} />
     <Route exact path='/login' render={Login} />
