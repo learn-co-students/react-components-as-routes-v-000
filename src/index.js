80e6ae1dcd2621da1e6dd5e8e2060f696da0775e
e-be-walk <e.be.walk@gmail.com>
2018-09-30 17:28:52 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//Import react-router functions
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import About from './about.js'
-import Login from './login.js'
+/* Add NavLink to importer */
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import Home from './home';
+import About from './about';
+import Login from './login'
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Home!</h1>
-      </div>
-      )
-    }
-  }
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+/* add the navbar component */
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+
+
 
 //changed from `<Home />` to have router coordinate what's displayed
 ReactDOM.render((
   <Router>
     <React.Fragment>
+      <Navbar />
       <Route path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/login" component={Login} />
