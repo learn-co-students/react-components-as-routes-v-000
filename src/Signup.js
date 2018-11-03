36b822b6b2966c3d76606b161737204b2c9ba750
nlisauskas <nlisausk@alumni.nd.edu>
2018-11-03 11:48:04 -0500

    Done.

---
+++ src/Signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Signup extends React.Component {
+  render() {
+    return (
+      <div>
+    <h2> Time to sign up! </h2>
+      </div>
+    )
+  }
+}
+
+export default Signup
