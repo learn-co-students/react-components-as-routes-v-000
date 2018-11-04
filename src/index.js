c47246cc617ffb37836b51fa367069394c38ffce
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-04 19:39:29 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,116 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
 
-ReactDOM.render(
-  <Home />,
+import Home from './home';
+import Login from './login';
+import About from './about';
+
+// Step 1. Import react-router functions
+// 6. Added Navlinks import 
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+// 7. Add NavLink styling
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+ 
+/* add the navbar component */
+const Navbar = () => {
+  return (
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>);
+}
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// };
+
+
+// 3. Adding different Routes (Abount and Login)
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// };
+
+// const Login = () => {
+//   return (
+//     <div>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username" />
+//           <label htmlFor="username">Username</label>
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password" />
+//           <label htmlFor="password">Password</label>
+//         </div>
+//         <input type="submit" value="Login" />
+//       </form>
+//     </div>
+//   );
+// };
+
+// Step 2. Changed to have router coordinate what is displayed
+
+// 4. Added About and Login paths, wrapped in React.Fragment
+// 5. Moved routes to own js files, updated <Route> from render=... to component=...
+
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <Home />,
+//   document.getElementById('root')
+// );
