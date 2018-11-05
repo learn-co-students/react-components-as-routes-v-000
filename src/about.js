84229f0226c289a60e62a14fc45b0a89a68eb2f3
Chris Alberti <christopherjalberti@gmail.com>
2018-11-05 16:08:13 -0500

    Done.

---
+++ src/about.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is my about component!</h1>
+      </div>
+    );
+  };
+}
