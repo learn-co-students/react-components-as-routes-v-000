45d7bc7e15345471ec38cbb7a745f9ebe086f855
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-06 16:54:10 -0500

    Done.

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Home!</h1>
+      </div>
+    )
+  }
+}
+
+export default Home
