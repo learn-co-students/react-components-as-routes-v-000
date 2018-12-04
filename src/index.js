45dfb04a1414807379395a8253e8dec7ee43f6cb
Obafemi Fadairo <obafad94@gmail.com>
2018-12-03 20:18:12 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -9,7 +11,49 @@ const Home = () => {
   );
 };
 
-ReactDOM.render(
-  <Home />,
+    class Home extends React.Component {
+      render() {
+        return (
+          <div>
+            <h1>Home!</h1>
+          </div>
+        )
+      }
+    }
+ 
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
+ 
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Route path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
