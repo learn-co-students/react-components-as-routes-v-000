f82d12cf4d9666016fd46eadb51688344f724c16
Rygel-XVI <micha232@umn.edu>
2018-12-12 20:28:54 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import About from './about'
+import Login from './login'
+import Home from './home'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
 
-ReactDOM.render(
-  <Home />,
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+/* add the navbar component */
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route path="/" render={Home} />
+      <Route exact path="/about" render={About} />
+      <Route exact path="/login" render={Login} />
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
