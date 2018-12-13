ff3adf034642689f0224a6e8b0c3a45a9861c222
JCesana <joshuacesana@gmail.com>
2018-12-12 18:49:53 -0800

    Done.

---
+++ src/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Signup extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Signup Page</h2>
+      </div>
+    )
+  }
+}
+
+export default Signup;
