146c58bb8bc9488da7c652877123db8db5a3ac1b
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-29 17:57:20 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './home.js'
+import About from './about.js'
+import Login from './login.js'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
-
-const About = () => {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  )
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white'
 }
 
-const Login = () => {
-  return (
-    <div>
-      <form>
-        <div>
-          <input type='text' name='username' placeholder='Username' />
-          <label htmlFor='username'>Username</label>
-        </div>
-        <div>
-          <input type='password' name='password' placeholder='Password' />
-          <label htmlFor='password'>Password</label>
-        </div>
-        <input type='submit' value='Login' />
-      </form>
-    </div>
-  )
+const Navbar = () => {
+  <div>
+  <NavLink
+    to='/'
+    exact
+    style={link}
+    activeStyle={{
+      background: 'darkblue'
+    }}
+  >Home</NavLink>
+  <NavLink
+    to='/about'
+    exact
+    style={link}
+    activeStyle={{
+      background: 'darkblue'
+    }}
+  >About</NavLink>
+  <NavLink
+    to='/login'
+    exact
+    style={link}
+    activeStyle={{
+      background: 'darkblue'
+    }}
+  >Login</NavLink>
 }
+
+const Home = () => <h1>Home!</h1>;
+
+const About = () => <h1>This is my about component!</h1>;
+
+const Login = () =>
+  <form>
+    <h1>Login</h1>
+    <div>
+      <input type="text" name="username" placeholder="Username" />
+      <label htmlFor="username">Username</label>
+    </div>
+    <div>
+      <input type="password" name="password" placeholder="Password" />
+      <label htmlFor="password">Password</label>
+    </div>
+    <input type="submit" value="Login" />
+  </form>;
 
 ReactDOM.render((
   <Router>
     <>
-      <Route exact path='/' render={Home} />
-      <Route exact path='/about' render={About} />
-      <Route exact path="/login" render={Login} />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path="/login" component={Login} />
     </>
   </Router>),
   document.getElementById('root')
