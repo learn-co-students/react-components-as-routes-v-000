e0b7c7b58d27191e57cb961d266d649da466143f
github username <github email address>
2019-02-21 21:05:44 -0800

    Done.

---
+++ src/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Message extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Message!</h1>
+      </div>
+    )
+  }
+}
