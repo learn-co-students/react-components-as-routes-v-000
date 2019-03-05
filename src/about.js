2dde039412e34ccc2642f53eb27c6f4c31d62d19
MiriamPeskowitz <miriam5000@gmail.com>
2019-03-05 18:25:43 -0500

    Done.

---
+++ src/about.js
@@ -0,0 +1,11 @@
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About page</h1>
+      </div>
+    );
+  };
+}
+
---
+++ src/home.js
@@ -0,0 +1,9 @@
+class Home extends React.Component {
+  render() {
+    return (
+    <div>
+      <h1>Homes! Homes!</h1>
+    </div>
+  );
+};
+}
