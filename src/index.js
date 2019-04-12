f8df51d05b19e8c28a9b6d032524dede00abc375
Brad Schrag <brad.schrag@gmail.com>
2019-04-12 06:53:24 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import react-router functions
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -9,7 +11,40 @@ const Home = () => {
   );
 };
 
+const About = () => {
+	return (
+		<div>
+			<h1>This is my about component!</h1>
+		</div>
+	);
+}
+
+const Login = () => {
+	return (
+		<div>
+			<form>
+				<div>
+					<input type="text" name='username' placeholder='Username' />
+					<label htmlFor="username">Username</label>
+				</div>
+				<div>
+					<input type="password" name='password' placeholder='Password'/>
+					<label htmlFor="password">Password</label>
+				</div>
+				<input type="submit" value="Login" />
+			</form>
+		</div>
+	);
+}
+
+// use Router tag to wrap the render of home
 ReactDOM.render(
-  <Home />,
+	<Router>
+		<React.Fragment>
+			<Route path='/' render={Home} />
+			<Route exact path="/about" render={About} />
+			<Route exact path="/login" render={Login} />
+		</React.Fragment>
+	</Router>,
   document.getElementById('root')
 );
