0e011134887810c2cae5c7b2d06cd57582121fcc
github username <github email address>
2019-05-02 23:06:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -17,70 +17,64 @@ const link = {
 const Navbar = () =>
   <div>
 
-    // 
-    // <NavLink
-    //   to="/"
-    //   exact
-    //   style={link}
-    //   activeStyle={{
-    //     background: 'darkblue'
-    //   }}
-    //   >Home</Navlink>
-
       <NavLink
       to="/"
-      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
       exact
-      /* add styling to Navlink */
       style={link}
-      /* add prop for activeStyle */
       activeStyle={{
         background: 'darkblue'
       }}
-    >Home</NavLink>
+      >Home</NavLink>
+
+      <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+      >About</NavLink>
+
+      <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+      >Login</NavLink>
 
 
   </div>
 
+
 const Home = () => <h1>Home!</h1>;
 
-const Login = () => {
-  return (
-    <div>
+const About = () => { <h1>This is my about component!</h1>;
+
+const Login = () =>
       <form>
-        <div>
           <input type="text" name="username" placeholder="Username" />
           <label htmlFor="username">Username</label>
-        </div>
     <div>
       <input type="password" name="password" placeholder="Password" />
       <label htmlFor="password">Password</label>
     </div>
     <input type="submit" value="Login" />
-    </form>
-    </div>
-  )
-}
+    </form>;
 
-const About = () => {
-  return (
-      <div>
-        <h1>This is my about component!</h1>
-      </div>
-  )
-}
+
 
 // Step 2. Changed to have router coordinate what is displayed
 
-ReactDOM.render( (
+ReactDOM.render((
   <Router>
     <React.Fragment>
-      <Route path="/" component={Home} />
+      <Navbar />
+      <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/login" component={Login} />
     </React.Fragment>
   </Router>),
-
-  // <Home />,
   document.getElementById('root')
 );
