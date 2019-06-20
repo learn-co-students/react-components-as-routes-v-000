d977cce6d5ffc61bd4333cf90614e001b42a2ae7
Marc Mills <mdmills1990@gmail.com>
2019-06-20 16:33:10 -0600

    done.

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+class About extends React.Component {
+  render(){
+    return (
+      <div>
+        <h2>This is about my component!</h2>
+      </div>
+    )
+  }
+}
+
+export default About
