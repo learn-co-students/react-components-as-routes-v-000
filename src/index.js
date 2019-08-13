11641d43ad1d41a197df47b7827ac46301c2c016
cbshields <cbshields@outlook.com>
2019-08-13 10:57:09 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
+import About from './about'
+import Login from './login'
+import link from './link'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+const Navbar = () =>
+  <div>
+    <NavLink to="/"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'}}
+    >Home</NavLink>
 
-ReactDOM.render(
-  <Home />,
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}>About</NavLink>
+
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>
+
+
+class Home extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Home!</h1>
+      </div>
+    )
+  }
+}
+
+
+
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
---
+++ src/link.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+export default link
