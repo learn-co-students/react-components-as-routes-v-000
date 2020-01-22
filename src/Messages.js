943fd9549471ab860ef8b8df937ca707e5435879
gitluky <jluk87@gmail.com>
2020-01-21 19:34:00 -0500

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Messages extends React.Component {
+  render() {
+    return(
+      <div>
+        <div>
+          <h1>Messages</h1>
+        </div>
+        <div>
+          <p>Voluptate ea fugiat, nescius velit veniam iudicem legam ita ad id sint
+            incididunt eu quorum ingeniis quamquam, dolor nostrud ea instituendarum,
+            pariatur tempor anim ut amet, do minim tempor ab fabulas qui est te fugiat duis
+            summis. Qui aute a export ab qui tamen pariatur voluptatibus. Ex nam tempor
+            laborum, expetendis voluptatibus ex pariatur.</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Messages;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+import Messages from './Messages'
+import SignUp from './Signup'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <Router>
+    <divt>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/messages" component={Messages} />
+    </divt>
+  </Router>),
   document.getElementById('root')
 );
