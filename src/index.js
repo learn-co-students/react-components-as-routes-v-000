b715e96c0c165d483719e33a14622c161d08a836
sdcro <sdcrouse91@gmail.com>
2020-08-04 00:35:51 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home';
+import About from './About';
+import Login from './Login';
+import Signup from './Signup';
+import Messages from './Messages';
+import Navbar from './Navbar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
-
-const About = () => {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  )
-}
-
-const Login = () => {
-  return (
-    <div>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
-        </div>
-        <input type="submit" value="Login" />
-      </form>
-    </div>
-  )
-}
-
 ReactDOM.render(
-  (<Router>
-     <div>
-       {/* <Route path="/" component={Home} /> (Desirable when I want the same header on multiple pages) */}
-       <Route exact path="/" component={Home} />
-       <Route exact path="/about" component={About} />
-       <Route exact path="/login" component={Login} />
-     </div>
-   </Router>),
+  (
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/messages" component={Messages} />
+      </div>
+    </Router>
+  ),
   document.getElementById('root')
 );
