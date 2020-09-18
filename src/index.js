095dc1c8c11a078316d5c67c017cadc5f525c07c
Kat Tochowicz <ktochow1@binghamton.edu>
2020-09-18 15:04:33 +0200

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
+const link = {
+  width: '100 px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar = () => {
+  return(<div>
+    <NavLink to="/" exact style={link} activeStyle={{background: 'darkblue'}} >Home</NavLink>
+    <NavLink to="/about" exact style={link} activeStyle={{background: 'darkblue'}}>About</NavLink>
+    <NavLink to="/login" exact style={link} activeStyle={{background: 'darkblue'}}>Login</NavLink>
+  </div>)
+}
+
+const About = () => {
+  return(
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
+
+const Login = () => {
+  return(
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placehold="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  )
+}
 const Home = () => {
   return (
     <div>
@@ -9,7 +52,14 @@ const Home = () => {
   );
 };
 
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <Router>
+    <div>
+    <Navbar />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/login" component={Login} />
+    </div>
+  </Router>),
   document.getElementById('root')
 );
