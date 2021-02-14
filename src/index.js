704071cd56fdb77e1b1b89831127619c1d9a8b9a
RonSala <ron.sala@gmail.com>
2021-02-14 15:34:47 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home';
+import About from './About';
+import Login from './Login';
+import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const Home = () => {
+const Signup = () => {
   return (
     <div>
-      <h1>Home!</h1>
+      <p></p>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Signup" />
+      </form>
     </div>
-  );
-};
+  )
+}
+
+const Messages = () => <h1>This is my messages component!</h1>
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/messages" component={Messages} />
+    </div>
+  </Router>,
+   document.getElementById('root')
 );
