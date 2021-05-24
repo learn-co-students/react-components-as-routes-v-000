62172428f26014fefa0401fd98a6b67e725339ea
emanao <emanao@me.com>
2021-05-23 23:32:15 -0400

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,10 @@
+// src/About.js
+import React from 'react';
+ 
+class Messages extends React.Component {
+  render() {
+    return <h1>This is my about messages!</h1>;
+  }
+}
+ 
+export default Messages;
