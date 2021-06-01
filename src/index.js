174f11ee61ec4c135817ef8e4b5508e9e79e3cf7
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-01 18:56:25 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,124 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Signup from './Signup'
+import Messages from './Messages'
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const Home = () => {
-  return (
+// /* Add basic styling for NavLinks */
+// const link = {
+//   width: '100px',
+//   padding: '12px',
+//   margin: '0 6px 6px',
+//   background: 'blue',
+//   textDecoration: 'none',
+//   color: 'white',
+// }
+
+// /* add the navbar component */
+// const Navbar = () =>
+//   <div>
+//     <NavLink
+//       to="/"
+//       /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+//       exact
+//       /* add styling to Navlink */
+//       style={link}
+//       /* add prop for activeStyle */
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Home</NavLink>
+//     <NavLink
+//       to="/about"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >About</NavLink>
+//     <NavLink
+//       to="/login"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Login</NavLink>
+//     <NavLink
+//       to="/signup"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Signup</NavLink>
+//     <NavLink
+//       to="/messages"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Messages</NavLink>
+//   </div>;
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// };
+// const Home = () => <h1>Home!</h1>;
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// };
+
+// const About = () => <h1>This is my about component!</h1>;
+
+// const Login = () => {
+//   return (
+//     <div>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username" />
+//           <label htmlFor="username">Username</label>
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password" />
+//           <label htmlFor="password">Password</label>
+//         </div>
+//         <input type="submit" value="Login" />
+//       </form>
+//     </div>
+//   );
+// };
+
+// const Signup = () => <h2>Sign up here</h2>;
+
+// const Messages = () => <h2>Some messages for you</h2>;
+
+ReactDOM.render((
+  // <Home />,
+  <Router>
     <div>
-      <h1>Home!</h1>
+      {/* Add Navbar */}
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/messages" component={Messages} />
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
 );
